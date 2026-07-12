6d120ea522ad679b1a98bfa7c26b1c9c4020a8a9
jakubthebang <155679991+jakubthebang@users.noreply.github.com>
2026-07-12 23:21:40 +0200

    Create src/modules/farming/CropPlanner.ts

---
+++ src/modules/farming/CropPlanner.ts
@@ -0,0 +1,38 @@
+export class CropPlanner {
+
+
+
+plan(
+crop:string
+){
+
+
+return {
+
+
+crop,
+
+
+steps:[
+
+"scan farm",
+
+"harvest",
+
+"replant",
+
+"store items"
+
+]
+
+
+
+};
+
+
+
+}
+
+
+
+}
