192355546d772dd06f82d5caf3097877a69f2103
jakubthebang <155679991+jakubthebang@users.noreply.github.com>
2026-07-12 23:04:36 +0200

    Create src/integrations/voice/SpeechToText.ts

---
+++ src/integrations/voice/SpeechToText.ts
@@ -0,0 +1,26 @@
+export class SpeechToText {
+
+
+async convert(
+audio:any
+){
+
+
+/*
+Tu neskôr napojíme:
+
+- OpenAI Whisper
+- Google Speech API
+- Local Whisper model
+
+*/
+
+
+return String(audio);
+
+
+}
+
+
+
+}
