e761c8e690966b98210d1439364bd3ddabda08be
jakubthebang <155679991+jakubthebang@users.noreply.github.com>
2026-07-13 18:41:21 +0200

    Create EnvironmentScanner.ts

---
+++ src/modules/environment/EnvironmentScanner.ts
@@ -0,0 +1,107 @@
+import { Bot } from "mineflayer";
+import Vec3 from "vec3";
+
+
+export class EnvironmentScanner {
+
+
+    private bot: Bot;
+
+
+    constructor(
+        bot: Bot
+    ){
+
+        this.bot = bot;
+
+    }
+
+
+
+
+    scan(){
+
+
+        const pos =
+            this.bot.entity.position;
+
+
+
+        return {
+
+
+            front: this.checkBlock(
+                pos.offset(0,0,1)
+            ),
+
+
+            back: this.checkBlock(
+                pos.offset(0,0,-1)
+            ),
+
+
+            left: this.checkBlock(
+                pos.offset(-1,0,0)
+            ),
+
+
+            right: this.checkBlock(
+                pos.offset(1,0,0)
+            ),
+
+
+            below: this.checkBlock(
+                pos.offset(0,-1,0)
+            )
+
+
+        };
+
+
+    }
+
+
+
+
+
+
+
+    private checkBlock(
+        pos:Vec3
+    ){
+
+
+        const block =
+            this.bot.blockAt(pos);
+
+
+
+        if(!block){
+
+            return {
+                solid:false,
+                name:"air"
+            };
+
+        }
+
+
+
+
+        return {
+
+            solid:
+                block.boundingBox !== "empty",
+
+            name:
+                block.name
+
+        };
+
+
+    }
+
+
+
+
+}
