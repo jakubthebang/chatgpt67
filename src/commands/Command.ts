b32d224e2184f1c2507340cba9f5ca3fc2f8013f
jakubthebang <155679991+jakubthebang@users.noreply.github.com>
2026-07-13 12:14:50 +0200

    Update Command.ts

--- src/commands/Command.ts
+++ src/commands/Command.ts
@@ -1,14 +1,17 @@
 import { Bot } from "mineflayer";
 
+
 export interface Command {
 
     name: string;
 
     description: string;
 
+
     execute(
         bot: Bot,
-        args: string[]
+        args: string[],
+        username: string
     ): void;
 
 }
