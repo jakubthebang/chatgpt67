c89f61e9ef30493aff0710c0f8723562ae2072f9
jakubthebang <jakubmacak11@gmail.com>
2026-07-13 17:39:26 +0200

    Add vanilla auto jump controller

--- src/modules/movement/JumpController.ts
+++ src/modules/movement/JumpController.ts
@@ -1,96 +1,55 @@
 import { Bot } from "mineflayer";
-
+import { Vec3 } from "vec3";
 
 export class JumpController {
-
-
     private bot: Bot;
+    private interval?: NodeJS.Timeout;
 
-
-    constructor(bot: Bot){
-
+    constructor(bot: Bot) {
         this.bot = bot;
-
-        this.start();
-
     }
 
-
-
-    private start(){
-
-
-        this.bot.on(
-            "physicsTick",
-            ()=>{
-
-
-                const block =
-                    this.bot.blockAtCursor(2);
-
-
-
-                const velocity =
-                    this.bot.entity.velocity;
-
-
-
-                if(!block)
-                    return;
-
-
-
-                const front =
-                    this.bot.entity.position
-                        .offset(
-                            0,
-                            0,
-                            0
-                        );
-
-
-
-                const y =
-                    this.bot.entity.position.y;
-
-
-
-                // ak je pred botom blok vo výške nôh
-                // a bot ide dopredu -> vyskoč
-
-                if(
-                    block.position.y >= y - 1 &&
-                    velocity.x !== 0 ||
-                    velocity.z !== 0
-                ){
-
-                    this.bot.setControlState(
-                        "jump",
-                        true
-                    );
-
-
-                    setTimeout(()=>{
-
-
-                        this.bot.setControlState(
-                            "jump",
-                            false
-                        );
-
-
-                    },300);
-
-
-                }
-
-
-            }
-
-        );
-
-
+    start() {
+        this.interval = setInterval(() => {
+            this.checkAutoJump();
+        }, 100);
     }
 
+    stop() {
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
+    }
 
+    private checkAutoJump() {
+        const bot = this.bot;
+
+        if (!bot.entity) return;
+
+        const pos = bot.entity.position;
+
+        // blok pred botom
+        const forward = bot.entity.yaw;
+
+        const x = Math.floor(pos.x - Math.sin(forward));
+        const y = Math.floor(pos.y);
+        const z = Math.floor(pos.z + Math.cos(forward));
+
+        const block = bot.blockAt(new Vec3(x, y, z));
+        const above = bot.blockAt(new Vec3(x, y + 1, z));
+
+        if (!block || !above) return;
+
+        // 1 block prekážka + miesto nad ňou voľné
+        if (
+            block.boundingBox === "block" &&
+            above.boundingBox === "empty"
+        ) {
+            bot.setControlState("jump", true);
+
+            setTimeout(() => {
+                bot.setControlState("jump", false);
+            }, 250);
+        }
+    }
 }
